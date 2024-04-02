3f3c11fd35f8de722db78254d0d8add63a644932
esteru432 <164496788+esteru432@users.noreply.github.com>
2024-04-02 09:28:05 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,13 @@
+n=int(input("enter the year"))
+if(n%4==0 and n%100!=0):
+    {
+    print("leap year")
+    }
+elif(n%100==0 and n%400==0):
+    {
+        print("leap year")
+    }
+else:
+    {
+        (print("not a leap year"))
+     }
